d0e654daa0c130e0d872ce8cc86e5418fd60420e
Abdul musavir chukkan <musavirc17@gmail.com>
2023-09-03 16:09:54 +0530

    admin middleware done

--- routes/admin.php
+++ routes/admin.php
@@ -9,17 +9,20 @@ use Illuminate\Support\Facades\Route;
 route::name('admin.')->prefix('admin')->controller(LoginController::class)->group(function () {
     Route::get('login', 'login')->name('login');
     Route::post('do-login', 'doLogin')->name('do.login');
-    Route::get('logout', 'logout')->name('logout');
 
-    Route::get('dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+    Route::middleware('auth:admin')->group(function () {
+        Route::get('logout', 'logout')->name('logout');
 
-    route::name('products.')->prefix('products')->controller(ProductController::class)->group(function () {
-        Route::get('/', 'list')->name('list');
-        Route::get('create', 'create')->name('create');
-        Route::post('save', 'save')->name('save');
-        Route::get('details/{id}', 'details')->name('details');
-        Route::get('edit/{id}', 'edit')->name('edit');
-        Route::post('update', 'update')->name('update');
-        Route::get('delete/{id}', 'delete')->name('delete');
+        Route::get('dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+
+        route::name('products.')->prefix('products')->controller(ProductController::class)->group(function () {
+            Route::get('/', 'list')->name('list');
+            Route::get('create', 'create')->name('create');
+            Route::post('save', 'save')->name('save');
+            Route::get('details/{id}', 'details')->name('details');
+            Route::get('edit/{id}', 'edit')->name('edit');
+            Route::post('update', 'update')->name('update');
+            Route::get('delete/{id}', 'delete')->name('delete');
+        });
     });
 });
