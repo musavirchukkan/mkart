977d9773cf9a1371ab76ec64690947747f4ed99c
musavir chukkan <musavirc17@gmail.com>
2023-09-15 18:33:25 +0530

    15/09/2023

--- app/Http/Controllers/User/LoginController.php
+++ app/Http/Controllers/User/LoginController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\UserLoginRequest;
 use App\Http\Requests\UserSignUpRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController
 {
